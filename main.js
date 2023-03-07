84cf7d2534571802d8d398b20a1e4fa557361646
Filip Drábek <drabekfilip@seznam.cz>
2023-03-07 17:59:00 +0100

    adding event listener to window so when user click somewhere else than navbar navbar closes

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
 function toggleSidebar() {
   document.body.classList.toggle("open");
 }
+
+window.addEventListener("click", (e) => {
+  if (!e.target.closest(".sidebar")) {
+    document.body.classList.remove("open");
+  }
+});
